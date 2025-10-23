d963aee59dff36f18beef8c57d4bec2d78100c0a
“Alexis” <alexisbalboteo15@gmail.com>
2025-10-23 21:25:25 +0200

    w

---
+++ BalboteoAlexis3.10/src/ejercicio15/Ejercicio15.java
@@ -0,0 +1,39 @@
+/*
+ * 
+ * 
+ * 
+ */
+
+
+package ejercicio15;
+
+import java.util.Scanner;
+
+public class Ejercicio15 {
+
+	public static void main(String[] args) {
+		
+		int n,m;
+		Scanner teclado =new Scanner(System.in);
+		
+		
+		
+		
+		System.out.print("Dame dos valores.\n1º: ");
+		n = teclado.nextInt();
+		System.out.print("2º: ");
+		m = teclado.nextInt();
+		
+		int[] table = new int[n];
+		
+		array
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
