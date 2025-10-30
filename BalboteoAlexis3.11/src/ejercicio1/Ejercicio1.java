f004a8a24bc3386a96242b015b57e1715a2f10c5
“Alexis” <alexisbalboteo15@gmail.com>
2025-10-30 21:28:00 +0100

    a

---
+++ BalboteoAlexis3.11/src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,26 @@
+package ejercicio1;
+
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		
+		
+		int[][] numeros = new int[5][5];
+		
+		for(int i =0 ; i<5 ; i++) {
+			
+			for(int j = 0; j<5 ; j++) {
+				
+			}
+			
+		}
+		
+		
+		
+		
+		
+
+	}
+
+}
