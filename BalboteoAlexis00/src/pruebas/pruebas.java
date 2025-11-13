24a553563cf567e3736be55af1bc64760328a2b1
“Alexis” <alexisbalboteo15@gmail.com>
2025-11-13 20:09:07 +0100

    a

---
+++ BalboteoAlexis00/src/pruebas/pruebas.java
@@ -0,0 +1,16 @@
+package pruebas;
+
+import java.util.Scanner;
+import BibliotecaAlexis.PrimerosMetodos.PrimeroMetodos;
+
+public class pruebas {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner teclado = new Scanner(System.in);
+		
+		mostrarMenu();
+	}
+
+}
