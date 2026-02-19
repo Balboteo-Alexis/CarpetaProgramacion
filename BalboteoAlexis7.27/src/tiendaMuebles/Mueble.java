e5226f4fb095ed7f091de4c4e2adada24bff4380
“Alexis” <alexisbalboteo15@gmail.com>
2026-02-19 20:44:55 +0100

    a

---
+++ BalboteoAlexis7.27/src/tiendaMuebles/Mueble.java
@@ -0,0 +1,62 @@
+package tiendaMuebles;
+
+public abstract class Mueble {
+
+	public static final double PRECIO_MAX_MUEBLE = 10000.00;
+	public static final double PRECIO_MIN_MUEBLE = 0.01;
+
+	private static int contador = 1;
+
+	protected final int identificador;
+	protected final String descripcion;
+
+	protected double precio;
+
+	// constructor
+
+	public Mueble(String descripcion, double precio) throws IllegalArgumentException {
+
+		if (precio < PRECIO_MIN_MUEBLE || precio > PRECIO_MAX_MUEBLE) {
+			throw new IllegalArgumentException("El precio no está en el rango permitido: XXX,XX");
+		}
+
+		this.identificador = contador;
+		this.descripcion = descripcion;
+		this.precio = precio;
+
+		contador++;
+	}
+
+	// Getters
+
+	public static double getPrecioMaxMueble() {
+		return PRECIO_MAX_MUEBLE;
+	}
+
+	public static double getPrecioMinMueble() {
+		return PRECIO_MIN_MUEBLE;
+	}
+
+	public static int getContador() {
+		return contador;
+	}
+
+	public int getIdentificador() {
+		return identificador;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	// toString
+
+	public String toString() {
+		return "tipo: "+ this.getClass().getSimpleName() + " identificador=" + identificador + ", descripcion=" + descripcion + ", precio=" + precio;
+	}
+
+}
