5e52c77a1d771147887fb035fcc34ca12db21cb7
“Alexis <alexisbalboteo15@gmail.com>
2026-04-28 23:38:11 +0200

    comit def

--- BalboteoAlexis9.31/src/ejercicioA4/Principal.java
+++ BalboteoAlexis9.31/src/ejercicioA4/Principal.java
@@ -7,18 +7,27 @@ public class Principal {
 	public static void main(String[] args) {
 
 		
-		File misCosas = new File("C:\\Users\\DAM1\\Documents\\Programación\\BalboteoAlexis9.31\\DOCS\\Mis cosas");
+		File misCosas = new File("DOCS\\Mis cosas");
 		
-		File alfabeto = new File("C:\\Users\\DAM1\\Documents\\Programación\\BalboteoAlexis9.31\\DOCS\\Alfabeto");
+		File alfabeto = new File("DOCS\\Alfabeto");
 		
 		misCosas.mkdir();
 		alfabeto.mkdir();
 		
 		
+		
+		File foto = new File("DOCS\\FOTOS");
+		
+		
+		File nuevo = new File("DOCS\\Mis cosas\\FOTOS");
+		
+		foto.renameTo( nuevo );
+		
+		
 		for (int i = 65; i < 91; i++) {
 			
 			
-			File nueva = new File("C:\\Users\\DAM1\\Documents\\Programación\\BalboteoAlexis9.31\\DOCS\\Alfabeto\\" +  (char)i );
+			File nueva = new File("DOCS\\Alfabeto\\" +  (char)i );
 			
 			nueva.mkdir();
 			
