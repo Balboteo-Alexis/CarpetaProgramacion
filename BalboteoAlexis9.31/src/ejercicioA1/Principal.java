ac1a3a6b76a62c2bd29dace435c15657b308d8c7
“Alexis <alexisbalboteo15@gmail.com>
2026-04-28 18:51:17 +0200

    sas

--- BalboteoAlexis9.31/src/ejercicioA1/Principal.java
+++ BalboteoAlexis9.31/src/ejercicioA1/Principal.java
@@ -1,32 +1,77 @@
 package ejercicioA1;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
 
 public class Principal {
 
 	public static void main(String[] args) {
 
+		Scanner teclado = new Scanner(System.in);
+		String texto;
+		
+		
+		
+		do {
+			
+			
+			System.out.print("Dame una ruta de archivos: ");
+			
+			texto = teclado.nextLine();
+			
+			if(!texto.equals("")) {
+				
+				File ruta = new File(texto);
+				
+				muestrainfoRuta(ruta);
+			}else {
+				System.out.println("fin");
+			}
+			
+			
+			
+			
+			System.out.println();
+		}while(!texto.equals(""));
+		
+		
 		
 		
 		
 		
 		
 	}
-	
-	
-	public void muestrainfoRuta(File ruta) {
-		
-		if(ruta.isFile()) {
+
+	public static void muestrainfoRuta(File ruta) {
+
+		if (ruta.isFile()) {
 			
-			System.out.println(ruta.toString());
+			System.out.println("Archivo");
+			System.out.println(    "A" + ruta.toString());
+
+		} else if (ruta.isDirectory()) {
+
 			
-		}else if(ruta.isDirectory()) {
-			System.out.println(ruta.toString());
 			
+			System.out.println("Directorio");
+			
+			
+			String[] lista = ruta.list();
+			
+			for (int i = 0; i < lista.length; i++) {
+				
+				
+				
+				System.out.println( "*" +lista[i].toString());
+			}
+			
+			
+		}else {
+			System.out.println("no es ni ruta ni archivo");
 		}
-		
+
 	}
-	
-	
 
 }
