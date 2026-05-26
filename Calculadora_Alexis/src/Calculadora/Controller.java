524f8bea700f7a3dd7523584417a20c09d9e280b
“Alexis <alexisbalboteo15@gmail.com>
2026-05-26 15:56:17 +0200

    comit def

--- Calculadora_Alexis/src/Calculadora/Controller.java
+++ Calculadora_Alexis/src/Calculadora/Controller.java
@@ -11,7 +11,7 @@ public class Controller {
     @FXML
     private Text output;
 
-    private long number1 = 0;
+    private long num1 = 0;
     private String operator = "";
     private boolean start = true;
 
@@ -37,14 +37,14 @@ public class Controller {
                 return;
 
             operator = value;
-            number1 = Long.parseLong(output.getText());
+            num1 = Long.parseLong(output.getText());
             output.setText("");
         }
         else {
             if (operator.isEmpty())
                 return;
 
-            output.setText(String.valueOf(model.calculate(number1, Long.parseLong(output.getText()), operator)));
+            output.setText(String.valueOf(model.calculate(num1, Long.parseLong(output.getText()), operator)));
             operator = "";
             start = true;
         }
